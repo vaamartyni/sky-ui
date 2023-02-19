23e660bacdbe88d19f4687837fbffd9856c79667
vaamartyni <116734686+vaamartyni@users.noreply.github.com>
2023-02-19 21:25:52 +0000

    Deploying to gh-pages from @ vaamartyni/sky-ui@652f7a9ebcb998ac47ad6031eea38e44602ac3cd 🚀

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import skytrackTheme from './skytrackTheme';
-
-addons.setConfig({
-    theme: skytrackTheme,
-});
--- .storybook/skytrackTheme.js
+++
@@ -1,6 +0,0 @@
-import { create } from '@storybook/theming';
-export default create({
-    base: 'light',
-    brandTitle: 'HEAVEN UI',
-    brandTarget: '_self',
-});
