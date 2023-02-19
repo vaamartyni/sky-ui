e177229e08a11ba161b81d58a2569adbf65331f6
Valdemar <matyniuk.vaa@gmail.com>
2023-02-19 11:06:57 -0800

    chore: still setting up

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import skytrackTheme from './skytrackTheme';
+
+addons.setConfig({
+    theme: skytrackTheme,
+});
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Thing, Props } from '../src';
+import { Button, IButton } from "../src/Button";
 
 const meta: Meta = {
-  title: 'Welcome',
-  component: Thing,
+  title: 'SKY Button',
+  component: Button,
   argTypes: {
     children: {
       control: {
@@ -12,17 +12,19 @@ const meta: Meta = {
       },
     },
   },
-  parameters: {
-    controls: { expanded: true },
-  },
 };
 
 export default meta;
 
-const Template: Story<Props> = args => <Thing {...args} />;
+const Template: Story<IButton> = args => <Button {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
-export const Default = Template.bind({});
+export const SKYButtonDefault = Template.bind({});
 
-Default.args = {};
+SKYButtonDefault.args = {
+  onClick:() => console.log('Sky button'),
+  children: 'SkyButton',
+  disabled: false,
+  variant: "primary"
+} as IButton;
