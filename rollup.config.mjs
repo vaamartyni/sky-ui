313ba0b71ff73d7741f23cdd299821884796537d
Vladimir Martyniuk <martyniuk.vaa@gmail.com>
2023-02-20 03:38:16 +0300

    feat(rollup): fix roll-up config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
@@ -19,7 +19,7 @@ export default {
         peerDepsExternal(),
         resolve(),
         commonjs(),
-        typescript({ useTsconfigDeclarationDir: true }),
+        typescript(),
         postcss({
             extensions: [".css"],
         }),
